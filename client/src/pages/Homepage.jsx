bbb577290bcadb9eb109f72ec922f51a911ba259
Jashan Saini <239391388+jasxhn@users.noreply.github.com>
2026-03-24 22:48:21 +0530

    Fix notes API requests to use deployed base URL

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
-import axios from "axios"
 import toast from "react-hot-toast"
 import NoteCard from '../components/NoteCard'
+import api from '../lib/axios'
 
 const Homepage = () => {
 
@@ -14,7 +14,7 @@ const Homepage = () => {
   useEffect(()=> {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes")
+        const res = await api.get('/notes')
         console.log(res.data);
         setNotes(res.data)
         setIsRateLimited(false)
